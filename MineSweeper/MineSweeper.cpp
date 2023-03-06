d04264665519713951431bb4fac1be705d5f8730
gros bg <vgautier@gaming.tech>
2023-03-06 08:49:34 +0100

    seghdg

--- MineSweeper/MineSweeper.cpp
+++ MineSweeper/MineSweeper.cpp
@@ -1,39 +1,380 @@
 // Démineur.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
-#include <iostream>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <math.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
 
 typedef struct Case {
     int content;
-    int state;
+    int isVisible; // 1 = visible, 0 = hidden;
     int indicator;
+    int flag = 0; //1 = drapeau, 0 = pas de drapeau
+    int current = 0; // position du joueur : si 1, le joueur est ici
 
 } Case;
 
+typedef struct Board {
+    Case* grid;
+    int size;
+    int remaining;
+    int iMinesAmount;
+} Board;
 
-int main()
+Board init(int size, int mines);
+void displayBoard(Board oBoard);
+void reveal(Board* table, int x, int y);
+void check(Board* table, int x, int y);
+void setFlag(Board* table, int x, int y);
+void generateMines(Board* oBoard);
+void oddToEvenByLower(int* number);
+void checkWin(Board* table, int x, int y, int* playing);
+void displayUI(Board* board, SDL_Window* window, SDL_Renderer* renderer);
+
+
+int main(int argc, char* argv[])
 {
 
-    int board[5][5];
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            printf("%d", board[i][j]);
+
+    int size;
+    printf("Entrez une taille de grille : ");
+    int res = scanf_s("%d", &size);
+
+    int iMinesAmount;
+    printf("Enter the amount of mines present on the grid : ");
+    scanf_s("%d", &iMinesAmount);
+
+    int difficultie = 0;
+    while (difficultie != 1 && difficultie != 2 && difficultie != 3)
+    {
+        printf("difficulté ? (1/2/3)");
+        res = scanf_s("%d", &difficultie);
+    }
+    difficultie = 6 / difficultie;
+
+
+    Board table = init(size, iMinesAmount); //round(size/difficultie));
+
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    TTF_Init();
+    int x;
+    int y;
+
+    window = SDL_CreateWindow("Une fenetre SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Création du renderer
+
+
+
+    int isPlaying = 1;
+
+
+    while (isPlaying == 1)
+    {
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {  // poll until all events are handled!
+            switch (event.type)
+            {
+            case SDL_MOUSEBUTTONDOWN:
+
+                x = floor(event.button.x / 20);
+                y = floor(event.button.y / 20);
+
+                if (event.button.button == 1) {
+                    reveal(&table, x, y);
+                    checkWin(&table, x, y, &isPlaying);
+                }
+                else if (event.button.button == 3) {
+                    setFlag(&table, x, y);
+                }
+                break;
+            }
         }
-        printf("\n");
+
+        displayUI(&table, window, renderer);
+
+
     }
 
-    printf("%d", board[2][2]);
-
-    // std::cout << "Hello World!\n";
-
-    int* bgfu = &board;
-
+    return 0;
 }
 
 
-//void displayBoard( Board board) {
 
-// }
+void displayUI(Board* board, SDL_Window* window, SDL_Renderer* renderer) {
+
+
+
+
+
+
+    SDL_Rect tile;
+    SDL_Color fontColor;
+    TTF_Font* vera = TTF_OpenFont("fonts/ttf-bitstream-vera-1.10/Vera.ttf", 16);
+    SDL_Surface* message;
+    char content[2];
+    SDL_Texture* indicTile;
+
+
+    for (int i = 0; i < board->size; i++) {
+        for (int j = 0; j < board->size; j++)
+        {
+
+            tile = { j * 20, i * 20 , 20, 20 };//{position x * la taille d'une case, position y * la taille d'une case, taille de la case (20 * 20)}
+
+            if (board->grid[i * 15 + j].flag == 1) {
+
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderFillRect(renderer, &tile);
+
+            }
+            else if (board->grid[i * 15 + j].isVisible == 0) {
+
+
+                if ((i * 15 + j) % 2 == 0) {
+                    SDL_SetRenderDrawColor(renderer, 160, 0, 160, 255);
+                }
+                else { SDL_SetRenderDrawColor(renderer, 150, 0, 150, 255); }
+
+
+                SDL_RenderFillRect(renderer, &tile);
+
+
+            }
+            else if (board->grid[i * 15 + j].isVisible == 1) {
+
+
+                if ((i * 15 + j) % 2 == 0) {
+                    SDL_SetRenderDrawColor(renderer, 70, 70, 70, 255);
+                }
+                else {
+                    SDL_SetRenderDrawColor(renderer, 80, 80, 80, 255);
+                }
+
+
+                if (board->grid[i * 15 + j].content == 0) {
+                    SDL_GetRenderDrawColor(renderer, &fontColor.r, &fontColor.g, &fontColor.b, &fontColor.a);
+                }
+                else if (board->grid[i * 15 + j].content == 1) {
+                    fontColor.r = 66;
+                    fontColor.g = 147;
+                    fontColor.b = 245;
+                    fontColor.a = 255;
+                }
+                else if (board->grid[i * 15 + j].content == 2) {
+                    fontColor.r = 144;
+                    fontColor.g = 66;
+                    fontColor.b = 245;
+                    fontColor.a = 255;
+                }
+                else if (board->grid[i * 15 + j].content >= 3) {
+                    fontColor.r = 201;
+                    fontColor.g = 8;
+                    fontColor.b = 8;
+                    fontColor.a = 255;
+                }
+
+
+                sprintf_s(content, "%d", board->grid[i * 15 + j].content);
+                SDL_RenderFillRect(renderer, &tile);
+                message = TTF_RenderText_Blended(vera, content, fontColor);
+                indicTile = SDL_CreateTextureFromSurface(renderer, message);
+                SDL_RenderCopy(renderer, indicTile, NULL, &tile);
+            }
+        }
+    }
+
+
+    SDL_RenderPresent(renderer);
+
+
+
+
+    //SDL_WaitEvent(event);
+    //SDL_Delay(1000);//pause de 3 secondes
+
+    ////on libère tout
+    ////SDL_FreeSurface(message);
+    //TTF_CloseFont(vera);
+    ////SDL_DestroyTexture(indicTile);
+    //SDL_DestroyRenderer(renderer);
+    //SDL_DestroyWindow(window);
+    //TTF_Quit();
+    //SDL_Quit(); 
+}
+
+void displayBoard(Board oBoard) {
+    for (int i = 0; i < oBoard.size; i++) {
+        for (int j = 0; j < oBoard.size; j++) {
+            if (oBoard.grid[i * oBoard.size + j].current == 1)
+            {
+                if (oBoard.grid[i * oBoard.size + j].flag == 1) {
+                    printf("\x1b[46mF\033[0;37m ");
+                }
+                else if (oBoard.grid[i * oBoard.size + j].isVisible == 0) {
+                    printf("\x1b[46mX\033[0;37m ");
+                }
+                else {
+                    printf("\x1b[46m%d\033[0;37m ", oBoard.grid[i * oBoard.size + j].content);
+                }
+            }
+            else {
+                if (oBoard.grid[i * oBoard.size + j].flag == 1) {
+                    printf("\033[0;32mF\033[0;37m ");
+                }
+                else if (oBoard.grid[i * oBoard.size + j].isVisible == 0) {
+                    printf("\033[0; 32mX\033[0;37m ");
+                }
+                else if (oBoard.grid[i * oBoard.size + j].content == 0) { // on sait que la case est visible
+                    printf("  ");
+                }
+                else {
+                    printf("\033[0;31m%d\033[0;37m ", oBoard.grid[i * oBoard.size + j].content);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+Board init(int size, int iMinesAmount) {
+    Board table;
+    table.size = size;
+    table.grid = (Case*)malloc(sizeof(Case) * table.size * table.size);
+    table.iMinesAmount = iMinesAmount;
+    table.remaining = size * size - iMinesAmount;
+
+
+    for (int j = 0; j < table.size * table.size; j++) {
+        table.grid[j].content = 0;
+        table.grid[j].isVisible = 0;
+    };
+
+
+    for (int j = 0; j < table.size * table.size; j++) {
+
+    }
+
+    //int r = time(NULL);
+    //int i = 0;
+    //while (iMinesAmount > 0) {
+    //    i++;
+    //    int x = abs(r % size);
+    //    r *= time(NULL) % 9; r += 1;
+    //    int y = abs(r % size);
+    //    r *= time(NULL) % 9;
+    //    if (table.grid[x + y * table.size].content == 0) {
+    //        table.grid[x + y * table.size].content = 9;
+    //        iMinesAmount -= 1;
+    //    }
+    //}
+    generateMines(&table);
+    displayBoard(table);
+    return table;
+}
+
+void generateMines(Board* oBoard) {
+    int iFreeCasesLength = oBoard->size * oBoard->size * 2;
+    int* freeCases = (int*)malloc(sizeof(int) * iFreeCasesLength);
+
+    for (int i = 0; i < oBoard->size; i++) {
+        for (int j = 0; j < oBoard->size; j++) {
+            freeCases[i * oBoard->size * 2 + j * 2] = i;
+            freeCases[i * oBoard->size * 2 + j * 2 + 1] = j;
+        }
+    }
+
+    int r = time(NULL);
+
+    for (int i = 0; i < oBoard->iMinesAmount; i++) {
+        int tmpLastFreeCase[2] = { freeCases[iFreeCasesLength - 2], freeCases[iFreeCasesLength - 1] };
+        int* variable = (int*)realloc(freeCases, sizeof(int) * iFreeCasesLength - 2);
+        if (variable != NULL) {
+            freeCases = variable;
+            iFreeCasesLength -= 2;
+            int randomIndex = r % iFreeCasesLength;
+            oddToEvenByLower(&randomIndex);
+            if (randomIndex != iFreeCasesLength - 1) {
+                oBoard->grid[freeCases[randomIndex] + freeCases[randomIndex + 1] * oBoard->size].content = 9;
+                freeCases[randomIndex] = tmpLastFreeCase[0];
+                freeCases[randomIndex + 1] = tmpLastFreeCase[1];
+            }
+        }
+    }
+    free(freeCases);
+}
+
+void oddToEvenByLower(int* number) {
+    if (*number % 2 == 1) {
+        (*number)--;
+    }
+}
+
+void reveal(Board* table, int x, int y) {
+    if (table->grid[x + y * table->size].content != 9 && table->grid[x + y * table->size].isVisible == 0) {
+        table->grid[x + y * table->size].isVisible = 1;
+        table->remaining -= 1;
+        check(table, x, y);
+        if (table->grid[x + y * table->size].content == 0) {
+            for (int i = -1; i < 2; i++) {
+                if (x + i >= 0 && x + i < table->size) {
+                    for (int j = -1; j < 2; j++) {
+                        if (y + j >= 0 && y + j < table->size) {
+                            if (table->grid[(x + i) + (y + j) * table->size].isVisible != 1) {
+                                reveal(table, x + i, y + j);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+}
+
+void check(Board* table, int x, int y) {
+    if (table->grid[x + y * table->size].content != 9)
+    {
+        for (int i = -1; i < 2; i++) {
+            if (x + i >= 0 && x + i < table->size) {
+                for (int j = -1; j < 2; j++) {
+                    if (y + j >= 0 && y + j < table->size) {
+                        if (table->grid[(x + i) + (y + j) * table->size].content == 9) {
+                            table->grid[x + y * table->size].content += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void setFlag(Board* table, int x, int y) {
+    printf("OK OK");
+    if (table->grid[(x + y * table->size)].flag == 1) {
+        table->grid[(x + y * table->size)].flag = 0;
+    }
+    else {
+        table->grid[(x + y * table->size)].flag = 1;
+    }
+}
+
+void checkWin(Board* table, int x, int y, int* playing) {
+    if (table->grid[x + y * table->size].content == 9)
+    {
+        printf("c'est lose\n");
+        *playing = 0;
+    }
+    else if (table->remaining == 0)
+    {
+        printf("you won, congrats boy\n");
+        *playing = 0;
+    }
+}
